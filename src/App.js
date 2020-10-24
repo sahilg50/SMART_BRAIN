7f1de6995b026e946e8721c8d812440a1923bd35
sahilg50 <56653026+sahilg50@users.noreply.github.com>
2020-10-25 03:11:03 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
 const particlesOptions = {
@@ -18,7 +19,35 @@ const particlesOptions = {
 	},
 };
 
+const app = new Clarifai.App({ apiKey: 'df41bf56be394d48aef6963cb05bb6b4' });
+
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+		};
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onButtonSubmit = () => {
+		console.log('click');
+		app.models
+			.predict(
+				Clarifai.GENERAL_MODEL,
+				'https://samples.clarifai.com/metro-north.jpg'
+			)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	render() {
 		return (
 			<div className="App">
@@ -26,7 +55,10 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImageLinkForm />
+				<ImageLinkForm
+					onInputChange={this.onInputChange}
+					onButtonSubmit={this.onButtonSubmit}
+				/>
 			</div>
 		);
 	}
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Rank = () => {
 	return (
-		<di>
+		<div>
 			<div className="white f3">{'Sahil, your rank is'}</div>
 			<div className="white f1">{'#5'}</div>
-		</di>
+		</div>
 	);
 };
 
