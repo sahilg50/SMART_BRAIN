ae2b7dc1256a18066c8e7ec291b41e43517260a6
sahilg50 <56653026+sahilg50@users.noreply.github.com>
2020-10-24 23:33:37 +0530

    added particles.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 100,
+			density: {
+				enable: true,
+				value_area: 800,
+			},
+		},
+	},
+};
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Particles className="particles" params={particlesOptions} />
 				<Navigation />
 				<Logo />
+				<Rank />
 				<ImageLinkForm />
 			</div>
 		);
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<di>
+			<div className="white f3">{'Sahil, your rank is'}</div>
+			<div className="white f1">{'#5'}</div>
+		</di>
+	);
+};
+
+export default Rank;
